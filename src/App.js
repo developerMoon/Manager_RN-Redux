39c2a5fd342f7b9dedd74f9c0b88043cf72adc51
Taylor Moon <taylor@Taylors-MacBook-Pro.local>
2018-05-30 11:30:13 +0900

    installed react-native-router-flux and created scene for login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
     componentWillMount(){
@@ -23,7 +24,7 @@ class App extends Component {
         return (
             //you need to provide at least one default reducer
             <Provider store={store}>
-                <LoginForm />
+                <Router />
             </Provider>
         );
     }
