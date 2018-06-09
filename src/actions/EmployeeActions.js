aebf7aa329fb2de9116fef58e0eba193297bb379
developerMoon <moonsoyoung24@gmail.com>
2018-06-09 21:42:16 +0900

    clean form after inserting data

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
-
+import { Actions } from 'react-native-router-flux';
 import {
-    EMPLOYEE_UPDATE
+    EMPLOYEE_UPDATE,
+    EMPLOYEE_CREATE
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -14,7 +15,14 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
     const { currentUser } = firebase.auth();
 
-    firebase.database().ref(`/users/${currentUser.uid}/employees`)
-        .push({ name, phone, shift });
-    
+    return (dispatch) => {
+        //put inserted shift info to firebase
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .push({ name, phone, shift })
+            .then(()=> {
+                dispatch({ type: EMPLOYEE_CREATE});
+                Actions.pop();
+        }); //.pop()for current version
+    }; //this will be called by redux thunk
+    //after saved to database - last data remains in form
 };
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,11 +1,12 @@
 import {
-    EMPLOYEE_UPDATE
+    EMPLOYEE_UPDATE,
+    EMPLOYEE_CREATE
 } from '../actions/types';
 
 const INITIAL_STATE = {
     name: '',
     phone: '',
-    shift: 'Monday'
+    shift: ''
 };
 
 export default (state = INITIAL_STATE, action ) => {
@@ -15,6 +16,9 @@ export default (state = INITIAL_STATE, action ) => {
             //reducer : function that returns some data
             //[action.payload.prop] 이 shift, name 등일 수 있다
             return { ...state, [action.payload.prop]: action.payload.value }
+        case EMPLOYEE_CREATE:
+            //to make form clean after insert data
+            return INITIAL_STATE;
         default: 
            return state;
     }
